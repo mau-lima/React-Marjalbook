ca4b4f18d70b9a905770d16a48d8f350c71113be
Mauricio Lima <limamauricio303@gmail.com>
2021-01-17 19:19:46 -0300

    Replaces the app function component with a class component

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+
+  render (){
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,8 +21,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </div>)
+  };
 }
 
 export default App;
