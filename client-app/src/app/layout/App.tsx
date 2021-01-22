b049c10edd55392d7e1fb949c89189a34e89d53a
Mauricio Lima <limamauricio303@gmail.com>
2021-01-22 20:20:03 -0300

    Refactors the app to a React function component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,40 +1,33 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Header, Icon, List } from "semantic-ui-react";
 import { IActivity } from "../modules/activity";
 
-interface IState{
-  activities: IActivity[];
-}
+const App = () => {
 
-class App extends Component<{},IState> { //1st param = prop type. 2nd param = state type
-  readonly state: IState = { //this is needed bc state should be modified by this.setState()
-    activities: []
-  };
+  const [activities, setActivities] = useState<IActivity[]>([]); //activities = the state itself, and setActivities is the function that sets it
 
-  componentDidMount() {
-    axios.get<IActivity[]>("http://localhost:5000/api/activities").then((response) => {
-      this.setState({
-        activities: response.data,
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
       });
-    });
-  }
+  },[]); //the [] argument prevents this from running over and over again
 
-  render() {
-    return (
-      <div>
-        <Header as="h2">
-          <Icon name="users" />
-          <Header.Content>Marjalbook v0.1</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((activity: IActivity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header as="h2">
+        <Icon name="users" />
+        <Header.Content>Marjalbook v0.1</Header.Content>
+      </Header>
+      <List>
+        {activities.map((activity: IActivity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
 
 export default App;
