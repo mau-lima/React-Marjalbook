20890b59c43621d913292877454f3c31934885cb
Mauricio Lima <limamauricio303@gmail.com>
2021-03-16 00:48:43 -0300

    Removes all reference to an old "submitting" state boolean

--- client-app/src/app/models/rootState.ts
+++ client-app/src/app/models/rootState.ts
@@ -6,7 +6,6 @@ export interface IRootState{
     counter: number;
     activities: IActivity[];
     loading: boolean;
-    submitting: boolean; //solia manejar si se esta submiteando o no el formulario. Se puede tneer como estaod local. destruir
     user: IUser | null;
     token:string;
     modal:IModalState;
--- client-app/src/reducers/index.ts
+++ client-app/src/reducers/index.ts
@@ -3,7 +3,6 @@ import activitiesReducer from './activities';
 
 import { combineReducers} from 'redux';
 import loadingReducer from './loading';
-import submittingReducer from './submitting';
 import userReducer from './user';
 import tokenReducer from './token';
 import modalReducer from './modal';
@@ -12,7 +11,6 @@ const rootReducer = combineReducers({
     counter: counterReducer,
     activities: activitiesReducer,
     loading: loadingReducer,
-    submitting: submittingReducer,
     user: userReducer,
     token: tokenReducer,
     modal: modalReducer
